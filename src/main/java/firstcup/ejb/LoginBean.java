3471cbb91b0ed8b57efa899118325b99682ec782
Guttojss <guttojsstfg@gmail.com>
2024-11-18 12:21:32 +0000

    + Login Front-end + Tried to make Back-end

---
+++ src/main/java/firstcup/ejb/LoginBean.java
@@ -0,0 +1,38 @@
+package firstcup.ejb;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Named
+@SessionScoped
+public class LoginBean implements Serializable {
+
+    private String userName;
+    private static final Logger logger = Logger.getLogger(LoginBean.class.getName());
+
+    public LoginBean() {
+        // Construtor vazio, necessário para o CDI gerenciar o bean
+    }
+
+    // Método para processar o nome do usuário
+    public String processUser() {
+        // Simula o processamento do nome do usuário
+        if (userName == null || userName.isEmpty()) {
+            userName = "Guest";  // Define "Guest" como nome padrão se não houver valor
+        }
+        logger.log(Level.INFO, "User logged in: {0}", userName);
+        return "/user.xhtml";  // Navega para a página user.xhtml
+    }
+
+    // Getter e Setter para o nome do usuário
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
